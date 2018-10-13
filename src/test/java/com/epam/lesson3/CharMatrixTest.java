e717da7cd1b380f41db9bf0b0497267a64cc10e4
Vergilon <vergilium.on@yandex.ru>
2018-10-13 07:16:44 +0300

    Added CharMatrixTest source code

---
+++ src/test/java/com/epam/lesson3/CharMatrixTest.java
@@ -0,0 +1,54 @@
+package com.epam.lesson3;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+/**
+ * CharMatrixTest description
+ *
+ * @author Roman Moderatov
+ * @version 1.0
+ * @since 1.8
+ */
+public class CharMatrixTest {
+    CharMatrix cm = new CharMatrix();
+
+    @Test
+    public void getEdgeStringTest() {
+        Assert.assertEquals("++--", cm.getEdgeString());
+    }
+
+    @Test
+    public void getLimitStringTest() {
+        Assert.assertEquals("+*0", cm.getLimitString(0, 0, 3));
+    }
+
+    @Test
+    public void getStringOfRowEvenCharTest() {
+        String[] strTest = cm.getStringOfRowEvenChar();
+        StringBuilder builder = new StringBuilder();
+        for (String s : strTest) {
+            builder.append(s);
+        }
+        String str = new String(builder);
+        Assert.assertEquals("+0+***000***-0-", str);
+    }
+
+    @Test
+    public void getStringOfColumnUnevenCharTest() {
+        String[] strTest = cm.getStringOfColumnUnevenChar();
+        StringBuilder builder = new StringBuilder();
+        for (String s : strTest) {
+            builder.append(s);
+        }
+        String str = new String(builder);
+        Assert.assertEquals("**00**00**", str);
+    }
+
+    @Test
+    public void getCheckerStringTest() {
+        Assert.assertEquals("+0+0000000-0-", cm.getCheckerString(true));
+        Assert.assertEquals("+0-0000000+0-", cm.getCheckerString(false));
+    }
+
+}
