2270ab3ac94928dc3d9ab88567ef43bdd6c6cbd5
Vergilon <vergilium.on@yandex.ru>
2018-11-28 22:35:46 +0300

    Refactored code (common method for crosswise shooting)

--- src/main/java/com/epam/BattleShip/HumanDifficult.java
+++ src/main/java/com/epam/BattleShip/HumanDifficult.java
@@ -49,28 +49,38 @@ public class HumanDifficult implements DifficultStrategy {
     }
 
     /**
+     * Method created to highlight a common part of the methods of shooting on the sides
+     * @param x is X axis cells value
+     * @param y is Y axis cells value
+     * @param t local counter
+     */
+    private void specShot(int x, int y, int t)
+    {
+        int[] a = new int[]{x, y};
+        computer.shot(a);
+        if (computer.isDamage(x, y - 1)) {
+            upShot(x, y - 1);
+            t = 0;
+        } else {
+            t++;
+        }
+        if (computer.isKill(computer.getX(), computer.getY())) {
+            savepoint = true;
+            shot(computer);
+        }
+        if (computer.isMiss(computer.getX(), computer.getY())) {
+            return;
+        }
+    }
+    /**
      * Methods for shooting from down to up. Part of crosswise algorithm.
      *
      * @param x is X axis cells value
      * @param y is Y axis cells value
      */
     private void upShot(int x, int y) {
-        int[] a = new int[]{x, y};
         if (y > 1 && !computer.isRepeat(x, y - 1) && u == 0) {
-            computer.shot(a);
-            if (computer.isDamage(x, y - 1)) {
-                upShot(x, y - 1);
-                u = 0;
-            } else {
-                u++;
-            }
-            if (computer.isKill(computer.getX(), computer.getY())) {
-                savepoint = true;
-                shot(computer);
-            }
-            if (computer.isMiss(computer.getX(), computer.getY())) {
-                return;
-            }
+            specShot(x, y - 1, u);
         } else {
             downShot(x, y + u);
         }
@@ -83,24 +93,10 @@ public class HumanDifficult implements DifficultStrategy {
      * @param y is Y axis cells value
      */
     private void downShot(int x, int y) {
-        int[] a = new int[]{x, y};
         if (y < 10 && !computer.isRepeat(x, y + 1) && d == 0) {
-            computer.shot(a);
-            if (computer.isDamage(x, y + 1)) {
-                downShot(x, y + 1);
-                d = 0;
-            } else {
-                d++;
-            }
-            if (computer.isKill(computer.getX(), computer.getY())) {
-                savepoint = true;
-                shot(computer);
-            }
-            if (computer.isMiss(computer.getX(), computer.getY())) {
-                return;
-            }
+            specShot(x, y + 1, d);
         } else {
-            westShot(x, y - d);
+            rightShot(x, y - d);
         }
     }
 
@@ -110,25 +106,11 @@ public class HumanDifficult implements DifficultStrategy {
      * @param x is X axis cells value
      * @param y is Y axis cells value
      */
-    private void westShot(int x, int y) {
-        int[] a = new int[]{x, y};
+    private void rightShot(int x, int y) {
         if (x < 10 && !computer.isRepeat(x + 1, y) && w == 0) {
-            computer.shot(a);
-            if (computer.isDamage(x + 1, y)) {
-                westShot(x + 1, y);
-                w = 0;
-            } else {
-                w++;
-            }
-            if (computer.isKill(computer.getX(), computer.getY())) {
-                savepoint = true;
-                shot(computer);
-            }
-            if (computer.isMiss(computer.getX(), computer.getY())) {
-                return;
-            }
+            specShot(x + 1, y, w);
         } else {
-            eastShot(x - w, y);
+            leftShot(x - w, y);
         }
     }
 
@@ -138,23 +120,9 @@ public class HumanDifficult implements DifficultStrategy {
      * @param x is X axis cells value
      * @param y is Y axis cells value
      */
-    private void eastShot(int x, int y) {
-        int[] a = new int[]{x, y};
+    private void leftShot(int x, int y) {
         if (x > 1 && !computer.isRepeat(x - 1, y) && e == 0) {
-            computer.shot(a);
-            if (computer.isDamage(x - 1, y)) {
-                eastShot(x - 1, y);
-                e = 0;
-            } else {
-                e++;
-            }
-            if (computer.isKill(computer.getX(), computer.getY())) {
-                savepoint = true;
-                shot(computer);
-            }
-            if (computer.isMiss(computer.getX(), computer.getY())) {
-                return;
-            }
+            specShot(x - 1, y, e);
         } else {
             savepoint = true;
             shot(computer);
